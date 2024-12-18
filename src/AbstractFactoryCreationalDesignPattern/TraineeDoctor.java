e3978aa52855c56800b6d623d4c26413b3d2d1b8
Trupti Patil <110106326+trupti-patil23@users.noreply.github.com>
2024-12-17 23:09:47 -0800

    Delete src/AbstractFactoryCreationalDesignPattern directory

--- src/AbstractFactoryCreationalDesignPattern/TraineeDoctor.java
+++
@@ -1,9 +0,0 @@
-package AbstractFactoryCreationalDesignPattern;
-
-public class TraineeDoctor implements Professions {
-
-	@Override
-	public void print() {
-		System.out.println("I am Trainee Doctor.");		
-	}
-}
